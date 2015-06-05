9c2248ee023d255606ab4f10a027288d0fb4f46d
jackzhao-mj <jackzhao.mj@gmail.com>
2015-06-05 13:48:25 -0700

    decrease the possible number of memory that an app can access to.

---
+++ MATLAB/mybeep.m
@@ -0,0 +1,8 @@
+function mybeep
+
+i = 20
+while(i > 0)
+    beep;
+    pause(0.15);
+    i = i - 1;
+end
--- MATLAB/proj0/launcher.m
+++ MATLAB/proj0/launcher.m
@@ -6,7 +6,7 @@ global s1 s2;
 
 cache_size = 512; % set cache size = 512B;(2MB)
 cacheline_size = 1; % set cache line size = 1B;(64B)
-app_mem = 50 * 2^10; % set memory accessing size = 50KB;should be: 100 * cache_size
+app_mem = 5 * 2^10; % set memory accessing size = 5KB;should be: 100 * cache_size
 num_cl = cache_size / cacheline_size; % total cache line number;
 num_memaccess = app_mem / cacheline_size; % shold be: 100 * num_cl
 set_size = 16; % this is a 16-way set assiciated, every set has 16 cache lines
@@ -17,14 +17,14 @@ hit  = 0;
 cl = zeros(1, num_cl); % cl stores the data of all cachelines;
 x = ones(1, num_cl); % x keeps track of the status of all cachelines;
 x = -1 * x; % the original x is all -1, meaning all cacheline are empty;
-MAX = 40; % MAX is the parameter for the isolation algorithm;
+% MAX = 10; % MAX is the parameter for the isolation algorithm;
 
 lru = 1;
 lru_stamp = ones(1, num_cl); % lru_stamp keeps track of least-used time of all cacheline;
 lru_stamp = -1 * lru_stamp;
 
 % n: each application will access the memory for millions of times;
-n = 5000;
+n = 20000;
 % TODO1: making a large outside loop, very n in a big setp-in, (50 or 100) and
 % get series fo hit.
 
@@ -39,6 +39,8 @@ n = 5000;
 % TODO4: thinking a way to generating the next random number very fast 
 % now: every time, have to look at the seed and generate every pair is slow
 % because i'm using the "seed" thing
+% I HAVE TO MAKE THE LOOP NUMBER >> THE POSSIBLE NUMBER OF MEMORY THAT A
+% APP CAN ACCESS TO
 
 s1 = zeros(1, n); % si is the access of app i, init to all-zero;
 s2 = zeros(1, n);
@@ -50,7 +52,9 @@ for i = 1 : n
     s2(i) = rand_temp(2);
     replace_1(s1(i));
     replace_1(s2(i));
-end 
+end
+
+mybeep;
 
 % the next is drawing the Probability density function and cumulative
 % distribution function of current accessing pattern
