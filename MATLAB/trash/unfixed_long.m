cae21c06d33efe8e1ed46907696e2b20177cd75e
jackzhao-mj <jackzhao.mj@gmail.com>
2015-06-26 02:01:22 -0700

    talked with Scott, woking on changing the scheduling

--- MATLAB/trash/unfixed_long.m
+++ MATLAB/trash/unfixed_long.m
@@ -1,4 +1,11 @@
-i = [3 4 5 9 3 4 5 9 10];
-for i = 1 : 2 : 50
-    a(i) = a(i) + 1;
+x = [3 4 5 9 3 4 5 9 10];
+% a(x) = zeros(1, pow2(1, 48));
+a(x) = zeros(1, 2^48);
+for i = 1 : numel(x)
+    xx = x(i);
+    if a(xx) == 1
+        a(xx) = a(xx) + 1;
+    else
+        a(x) = 1;
+    end
 end
