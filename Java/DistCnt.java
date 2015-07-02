ac46f41d692de16ecf435d5dc7b63543a5ff08b6
jackzhao-mj <jackzhao.mj@gmail.com>
2015-07-02 16:00:39 -0700

    working on the counting program

--- Java/DistCnt.java
+++ Java/DistCnt.java
@@ -2,20 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class DistCnt{
-	Map<String, Integer> = new HashMap<String, Integer>
+	
 
 	public static void main(String[] args) {
-		commandMap = new HashMap(); 
-		commandMap.put("SET_DISPLAY", 0xD0);
-		int value = commandMap.get("SET_DISPLAY");
-		System.err.println("0x" + Integer.toHexString(value));
-		
-		// File file = new File("pinatrace.out");
-		// FileReader fileReader = new FileReader(file);
-		// BufferedReader bufferedReader = new BufferedReader(fileReader);
-		// StringBuffer stringBuffer = new StringBuffer();
-		// String line;
-		// while ((line = bufferedReader.readLine()) != null) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
 
+		try {
+			File file = new File("pinatrace.out");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String line = new String();
+			line = "0x7fd54a74e2d3: W 0x7fff7b4b1f18";
+			while ((line = bufferedReader.readLine()) != null) {
+				if (line.equals("#eof"))
+					
+				String[] addrs = line.split(": W ");
+				String ip = addrs[0]; 
+				String mem = addrs[2];
+				if (map.containsKey(mem)) {
+					Integer cnt = map.get(mem);
+					map.put(mem, cnt + 1);
+				}
+				else
+					map.put(mem, 1);
+			}
+			fileReader.close();
+		}
+		
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
---
+++ Java/cntTry.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class cntTry {
+	public static void main(String[] args) {
+		Map<String, Integer> addrMap = new HashMap<String, Integer>();
+		addrMap.put("wocoa", 1);
+		addrMap.put("nima", 1);
+		System.out.println(addrMap);
+	}
+}
