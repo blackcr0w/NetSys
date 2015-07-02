a235057a5cf7362e2dbd9f0bf34ec9d8805889f1
jackzhao-mj <jackzhao.mj@gmail.com>
2015-07-01 22:46:34 -0700

    still working on the counting

--- Java/DistCnt.java
+++ Java/DistCnt.java
@@ -1,3 +1,21 @@
-class DistCnt {
-	
+import java.io.*;
+import java.util.*;
+
+public class DistCnt{
+	Map<String, Integer> = new HashMap<String, Integer>
+
+	public static void main(String[] args) {
+		commandMap = new HashMap(); 
+		commandMap.put("SET_DISPLAY", 0xD0);
+		int value = commandMap.get("SET_DISPLAY");
+		System.err.println("0x" + Integer.toHexString(value));
+		
+		// File file = new File("pinatrace.out");
+		// FileReader fileReader = new FileReader(file);
+		// BufferedReader bufferedReader = new BufferedReader(fileReader);
+		// StringBuffer stringBuffer = new StringBuffer();
+		// String line;
+		// while ((line = bufferedReader.readLine()) != null) {
+
+	}
 }
